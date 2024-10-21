ac5bf42fe19e58b47e9c7678a9d385ae9d99d3d7
marciointra <marcio.intra@aluno.senai.br>
2024-10-21 19:41:23 -0300

    projeto refeito do zero

---
+++ Projeto_SENAI_Event_Flow/src/main/java/com/mycompany/projeto_senai_event_flow/Projeto_SENAI_Event_Flow.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_senai_event_flow;
+
+/**
+ *
+ * @author marci
+ */
+public class Projeto_SENAI_Event_Flow {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
